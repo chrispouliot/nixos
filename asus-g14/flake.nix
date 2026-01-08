7401a19251276042bd5a79e837233ea7b871c0b8
Chris Pouliot <9087150+chrispouliot@users.noreply.github.com>
2026-01-07 16:41:55 -0800

    Update flake.nix

--- asus-g14/flake.nix
+++ asus-g14/flake.nix
@@ -12,15 +12,15 @@
         system = "x86_64-linux";
         specialArgs = { inherit nix-cachyos-kernel; };
         modules = [
-         # (
-         #   { pkgs, ... }:
-         #   {
-         #     boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v4;
-         #     nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned];
-         #     nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-         #     nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-         #   }
-         # )
+          (
+            { pkgs, ... }:
+            {
+              boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v4;
+              nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned];
+              nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
+              nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+            }
+          )
           ./configuration.nix # Your system configuration.
         ];
       };
