3a14c27c16071e08506673e19226cd04831db3e7
Chris Pouliot <9087150+chrispouliot@users.noreply.github.com>
2026-06-06 15:27:45 -0700

    Combine cache substituters, de duplicate modules

--- asus-g14/flake.nix
+++ asus-g14/flake.nix
@@ -2,7 +2,7 @@
   description = "My configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
     cardwire = {
       url = "github:opengamingcollective/cardwire";
@@ -10,51 +10,67 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     cardwire-toggle = {
-      url = "github:chrispouliot/cardwire-toggle";
-      #url = "path:/home/chris/Projects/cardwire-toggle";
+      #url = "github:chrispouliot/cardwire-toggle";
+      url = "path:/home/chris/Projects/cardwire-toggle";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     nix-gaming-edge = {
       url = "github:powerofthe69/nix-gaming-edge";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, nix-cachyos-kernel, cardwire, cardwire-toggle, nix-gaming-edge, ... }@inputs:
+  outputs = { nixpkgs, nix-cachyos-kernel, cardwire, cardwire-toggle, nix-flatpak, nix-gaming-edge, ... }@inputs:
     {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = {
-            inherit inputs nix-cachyos-kernel;
+            inherit inputs;
           };
           modules = [
-            # Cachyos Kernel
-            ({ pkgs, ... }:
+            # Temporary firefox fix for 26.05
               {
-                boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v4;
-                nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ];
-                nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-                nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-            })
-            # Cachyos Proton
-            ({ pkgs, ... }: {
-              nix.settings = {
-                substituters = [ "https://nix-cache.tokidoki.dev/tokidoki" ];
-                trusted-public-keys = [ "tokidoki:MD4VWt3kK8Fmz3jkiGoNRJIW31/QAm7l1Dcgz2Xa4hk=" ];
-              };
-
+                nixpkgs.overlays = [
+                (final: prev: {
+                  firefoxpwa-unwrapped = prev.firefoxpwa-unwrapped.overrideAttrs (old: {
+                    postInstall = (old.postInstall or "") + ''
+                      mkdir -p $out/lib/firefoxpwa
+                    '';
+                  });
+                })
+              ];
+            }
+            # Cachyos Kernel and proton
+            ({ pkgs, ... }:
+            {
               nixpkgs.overlays = [
+                nix-cachyos-kernel.overlays.pinned
                 nix-gaming-edge.overlays.proton-cachyos
               ];
 
+              nix.settings = {
+                extra-substituters = [
+                  "https://attic.xuyh0120.win/lantian"
+                  "https://nix-cache.tokidoki.dev/tokidoki"
+                ];
+                extra-trusted-public-keys = [
+                  "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+                  "tokidoki:MD4VWt3kK8Fmz3jkiGoNRJIW31/QAm7l1Dcgz2Xa4hk="
+                ];
+              };
+              
+              boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v4;
               programs.steam = {
                 enable = true;
                 extraCompatPackages = [ pkgs.proton-cachyos-x86_64-v3 ];
               };
             })
+            # Cardwire GPU switching and gnome toggle extension
             cardwire.nixosModules.default
             cardwire-toggle.nixosModules.default
+            nix-flatpak.nixosModules.nix-flatpak
             ./configuration.nix
           ];
         };
